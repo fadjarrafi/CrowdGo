b04000adda6077c9f7f5b65b89ba1b2818f45de1
fadjarrafi <dark.fir21@gmail.com>
2022-09-28 02:19:47 +0700

    add register user repository

--- main.go
+++ main.go
@@ -3,39 +3,13 @@ package main
 import (
 	"golangweb/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	// dsn := "root:@tcp(127.0.0.1:3306)/crowdgo?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is good!!")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("=============")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdgo?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -43,9 +17,11 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "test save data",
+	}
 
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
 
 }
