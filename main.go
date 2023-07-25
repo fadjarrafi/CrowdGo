ab0b4cac8f9126a9adde89ce1d67dc1b608e3484
Fadjar Irfan rafi <dark.fir21@gmail.com>
2023-07-25 20:21:05 +0700

    Update getCampaigns (WIP)

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golangweb/auth"
 	"golangweb/campaign"
 	"golangweb/handler"
@@ -36,13 +35,10 @@ func main() {
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
-	campaigns, _ := campaignService.FindCampaigns(17)
-
-	fmt.Println(len(campaigns))
-
 	// userService.SaveAvatar(1, "images/1avatar.png")
 
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
@@ -50,6 +46,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 
 	router.Run()
 
